96a553043a5bd9853fe8d134a1aa7bab4e92fd1c
Andrey Morozov <jastic7@yandex.ru>
2018-12-19 23:01:32 +0300

    Fix comment

--- ObjcWrappers/FileWatcherProtocols.swift
+++ ObjcWrappers/FileWatcherProtocols.swift
@@ -16,7 +16,7 @@ public protocol FileWatcherOutput: NSObjectProtocol {
 	/// Fires, when monitoring file is updated.
 	///
 	/// - Parameters:
-	///	  - from: The file watcher, which handles file refreshing.
+	///	  - fileWatcher: The file watcher, which handles file refreshing.
 	///   - type: Status of refresh result.
 	///   - data: Content from file.
 	func refreshDidOccur(from fileWatcher: FileWatcherInput, type: RefreshResultType, data: Data?)
