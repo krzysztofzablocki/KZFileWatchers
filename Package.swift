7829a877c29f38b6b6f3467cfd7796276394bb29
Constantino Tsarouhas <c@tsarouhas.eu>
2022-01-08 06:36:26 +0100

    Vends library product in Swift package manifest, fixing issue #25 (#26)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "KZFileWatchers",
+    products: [
+        .library(name: "KZFileWatchers", targets: ["KZFileWatchers"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
